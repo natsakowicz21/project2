feddbaa83040808ca1d2f8439faa5e653488e1ac
natsakowicz21 <90803358+natsakowicz21@users.noreply.github.com>
2022-05-10 12:58:02 -0500

    Update favorite-movies.service.ts

--- src/app/services/favorite-movies.service.ts
+++ src/app/services/favorite-movies.service.ts
@@ -9,14 +9,14 @@ export class FavoriteMoviesService {
 
   getFavoriteMovies(){
     return [
-      {"Id" : 1, "Title":"The Northman", "Year": "2022"},
-      {"Id" : 2, "Title":"he Batman", "Year": "2022"},
-      {"Id" : 3, "Title":"The Unbearable Weight of Massive Talent", "Year": "2022"},
-      {"Id" : 4, "Title":"Everything Everywhere All at Once", "Year": "2022"},
-      {"Id" : 5, "Title":"Fantastic Beasts: The Secrets of Dumbledore", "Year": "2022"},
-      {"Id" : 6, "Title":"Morbius", "Year": "2022"},
-      {"Id" : 7, "Title":"The Bad Guys", "Year": "2022"},
-      {"Id" : 8, "Title":"Uncharted", "Year": "2022"}
+       {"Id" : 1, "Title":"The Bad Guys", "Year": "2022"},
+      {"Id" : 2, "Title":"Big Hero 6", "Year": "2014"},
+      {"Id" : 3, "Title":"Inception", "Year": "2010"},
+      {"Id" : 4, "Title":"Mindhunters", "Year": "2017"},
+      {"Id" : 5, "Title":"Better Call Saul", "Year": "2020"},
+      {"Id" : 6, "Title":"Breaking Bad", "Year": "2013"},
+      {"Id" : 7, "Title":"The Imitation Game", "Year": "2014"},
+      {"Id" : 8, "Title":"Inventing Anna", "Year": "2022"}
     ];
   }
 }
