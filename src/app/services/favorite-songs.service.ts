30dc32cd0a8ee579a5bdf6147cbe5ca83086bc29
natsakowicz21 <90803358+natsakowicz21@users.noreply.github.com>
2022-05-10 13:15:07 -0500

    Update favorite-songs.service.ts

--- src/app/services/favorite-songs.service.ts
+++ src/app/services/favorite-songs.service.ts
@@ -13,10 +13,6 @@ export class FavoriteSongsService {
       {"Id" : 2, "Name":"BANKROLL", "Band": "Bockhamption ft. A$AP Rocky, A$AP Ferg"},
       {"Id" : 3, "Name":"Deweloper", "Band": "Tymek"},
       {"Id" : 4, "Name":"ULTRA VIOLET", "Band": "Thouxanbanfauni"}
-      {"Id" : 5, "Name":"CIGARETTES", "Band": "Amir Obe"}
-      {"Id" : 6, "Name":"Ugh! -Dooqu Remix", "Band": "dwilly, Dooqu"}
-      {"Id" : 7, "Name":"80s", "Band": "Tymek"}
-      {"Id" : 8, "Name":"Textacy", "Band": "Dion Timmer"}
     ];
   }
 }
